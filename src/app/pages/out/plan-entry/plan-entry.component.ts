12c0bec4169846323cca36a9080ff2cdfdfcdc0e
git <842101237@qq.com>
2018-06-08 17:33:09 +0800

    06-08

--- src/app/pages/out/plan-entry/plan-entry.component.ts
+++ src/app/pages/out/plan-entry/plan-entry.component.ts
@@ -17,25 +17,49 @@ export class PlanEntryComponent implements OnInit {
     public date = "Select start date";
     public total;
     public dayAmount;
+    public Plan;
     ngOnInit() {
         this.Language = localStorage.getItem("language");
         this.datas = JSON.parse(this.routerIonfo.snapshot.params["data"]);
         this.title = this.datas.title;
         this.data = this.datas.data;
         console.log(this.datas);
-        let json = [
-            { role: "外协", contacts: [{ title: "A", names: ["LIHUO", "YUXINSHUAI"] }, { title: "b", names: ["LIHUO", "YUXINSHUAI"] }] },
-            { role: "供应商", contacts: [{ title: "A", names: ["LIHUO", "YUXINSHUAI"] }, { title: "b", names: ["LIHUO", "YUXINSHUAI"] }] }
-        ];
+        if (this.data.plan == 'Yes') {
+            this.service.http_get('/api/OuterFactory/ShowPlan?code=' + this.data.code, false).subscribe((data: any) => {
+                if(data.msg="success"){
+                    this.Plan = data;
+                }
+                let totaloutgoing:number =0;
+                let totalreturn:number =0;
+                this.Plan.result.recording.forEach(element => {
+                    totaloutgoing += Number(element.outgoingCount);
+                    totalreturn += Number(element.backFactory);
+                });
+                this.Plan.result.recording.push({
+                    date:"Total",
+                    outgoingCount:totaloutgoing,
+                    backFactory:totalreturn,
+                    reduce:"-" + (totaloutgoing-totalreturn)
+                })
+                console.log(data)
+            })
+        }
     }
-    Next() {
+    next() {
         if (this.total > 0 && this.dayAmount > 0 && this.date != "Select start date") {
-            this.router.navigate(['selectingSuppliers', JSON.stringify({ total: this.total, date: this.date, dayAmount:this.dayAmount,code:this.data.code})])
-        }else{
+            this.router.navigate(['selectingSuppliers', JSON.stringify({ total: this.total, date: this.date, dayAmount: this.dayAmount, code: this.data.code })])
+        } else {
             alert("请填写完整信息！")
         }
     }
-
+    submit(){
+        if(this.total > 0 && this.date != "Select start date"){
+            let option="totalCount=" + this.total + "&startTime=" + this.date + "&code=" +  this.data.code
+            this.service.http_post("/api/OuterFactory/EntryDayOutSchedule", option,false).subscribe((data:any)=>{
+                
+            })
+        }
+    }
     backDate($event) {
         let obj = JSON.parse($event);
         if (obj.date) {
--- src/app/pages/out/selecting-suppliers/selecting-suppliers.component.ts
+++ src/app/pages/out/selecting-suppliers/selecting-suppliers.component.ts
@@ -1,18 +1,72 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AppService } from '../../../app.service';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
-  selector: 'app-selecting-suppliers',
-  templateUrl: './selecting-suppliers.component.html',
-  styleUrls: ['./selecting-suppliers.component.css']
+    selector: 'app-selecting-suppliers',
+    templateUrl: './selecting-suppliers.component.html',
+    styleUrls: ['./selecting-suppliers.component.css']
 })
 export class SelectingSuppliersComponent implements OnInit {
 
-  constructor() { }
-  public Language;
-  public title;
-  ngOnInit() {
-    this.Language = localStorage.getItem("language");
-    this.title = "Selecting Suppliers"
-  }
+    constructor(private service: AppService, private routerIonfo: ActivatedRoute, private router: Router) { }
+    public Language;
+    public title;
+    public datas;
+    public userface = [];
+    public data;
+    ngOnInit() {
+        this.data = JSON.parse(this.routerIonfo.snapshot.params["data"]);
+        this.Language = localStorage.getItem("language");
+        this.title = "Selecting Suppliers";
+        this.service.http_get('/api/OuterFactory/GetContacts', false).subscribe((data: any) => {
+            if (data.msg == "success") {
+                this.datas = data;
+            }
+
+        })
+    }
+    show(item, i) {
+        item.state = !item.state;
+        if (item.state) {
+            this.datas.result.contactsList.forEach((element, index) => {
+                if (i != index) element.state = false;
+            });
+
+        }
+
+    }
+    add(user, $event) {
+        $event.stopPropagation();
+        user.state = !user.state;
+        if (user.state) {
+            this.userface.push(user);
+        } else {
+            this.userface.forEach((element, index) => {
+                if (user.uid == element.uid) {
+                    this.userface.splice(index,1);
+                    return;
+                }
+            });
+        }
+
+    }
+    send() {
+        let fids = [];
+        this.userface.forEach((element)=>{
+            fids.push(element.uid);
+        })
+        let option = {
+            totalCount:this.data.total,
+            startTime:this.data.date,
+            dayCount:this.data.dayAmount,
+            code:this.data.code,
+            ofids:fids
+        }
+        this.service.http_post("/api/OuterFactory/EntryPlan", option,false).subscribe((data:any)=>{
+            if(data.msg=="录入成功"){
+               window.history.go(-2);
+            }
+        })
+    }
 
 }
