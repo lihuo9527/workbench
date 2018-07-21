34b72257e9f92a72ddb841ab7ce9c66976f0a8fa
git <842101237@qq.com>
2018-07-21 12:00:31 +0800

    07-21

--- src/app/pages/forms/macro-capacity/capacity-figure/capacity-figure.component.ts
+++ src/app/pages/forms/macro-capacity/capacity-figure/capacity-figure.component.ts
@@ -14,14 +14,17 @@ export class CapacityFigureComponent implements OnInit {
     public totalCapacity: number;
     public totalLoad: number;
     public total: number;
+    public lang;
     constructor() { }
 
     ngOnInit() {
+        this.lang = localStorage.getItem("language");
         if (this.dataA.length < 3) {
             for (let i = this.dataA.length; i < 3; i++) {
                 const obj: any = {};
                 obj.color = '#fff';
                 obj.value = 0;
+                obj.name = "";
                 this.dataA.push(obj);
             }
         }
@@ -30,6 +33,7 @@ export class CapacityFigureComponent implements OnInit {
                 const obj: any = {};
                 obj.color = '#fff';
                 obj.value = 0;
+                obj.name = "";
                 this.dataB.push(obj);
             }
         }
@@ -42,8 +46,59 @@ export class CapacityFigureComponent implements OnInit {
     createChart(x, y: any[]) {
         return {
             tooltip: {
-                trigger: 'axis',
+                triggerOn:'mousemove',
+                textStyle: { color: "#fff" },
+                position: ['50%', '10%'],
+                padding: [
+                    15,  // 上
+                    15, // 右
+                    15,  // 下
+                    15, // 左
+                ],
+                alwaysShowContent: false,
+                trigger: 'item',
+                formatter: (params) => {
+                    if (params.seriesName == "total") return null;
+                    if (this.lang == 'cn') {
+                        switch (params.seriesName) {
+                            case "TotalCapacity":
+                                params.seriesName = "总产能";
+                                break;
+                            case "NormalCapacity":
+                                params.seriesName = "正常产能";
+                                break;
+                            case "OtCapacity":
+                                params.seriesName = "加班产能";
+                                break;
+                            case "OutCapacity":
+                                params.seriesName = "外发产能";
+                                break;
+                            case "PoPlanLoading":
+                                params.seriesName = "实单已排负载";
+                                break;
+                            case "PoLoading":
+                                params.seriesName = "实单未排负载";
+                                break;
+                            case "VirPlanLoading":
+                                params.seriesName = "非实单已排负载";
+                                break;
+                            case "VirPoLoading":
+                                params.seriesName = "非实单未排负载";
+                                break;
+                        }
+
+                    }
+                    let color = params.color;//图例颜色
+                    let htmlStr = '<div style="color:#ffffff;">';
+                    // console.log(params.seriesName);
+                    htmlStr += '<span style="margin-right:5px;display:inline-block;width:10px;height:10px;border-radius:5px;background-color:' + color + ';"></span>';
+                    htmlStr += params.seriesName + '：' + params.value + '';
+                    htmlStr += '</div>';
+                    return htmlStr;
+                },
+
                 axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                    z: 3,
                     type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
                 },
             },
@@ -73,43 +128,49 @@ export class CapacityFigureComponent implements OnInit {
             }],
             series: [
                 {
-                    name: "产能",
+                    name: x[2].name,
                     color: [x[2].color],
                     type: 'bar',
                     stack: 'B',
                     data: [x[2].value]
                 },
                 {
+                    name: x[1].name,
                     color: [x[1].color],
                     type: 'bar',
                     stack: 'B',
                     data: [x[1].value],
                 },
                 {
+                    name: x[0].name,
                     type: 'bar',
                     stack: 'B',
                     data: [x[0].value],
                     color: [x[0].color],
                 },
                 {
+                    name: y[2].name,
                     type: 'bar',
                     color: [y[2].color],
                     stack: 'C',
                     data: [y[2].value],
                 },
                 {
+                    name: y[1].name,
                     type: 'bar',
                     color: [y[1].color],
                     stack: 'C',
                     data: [y[1].value],
                 },
                 {
+                    name: y[0].name,
                     type: 'bar',
                     color: [y[0].color],
                     stack: 'C',
                     data: [y[0].value],
                 },
                 {
+                    name: "total",
                     type: 'bar',
                     stack: 'D',
                     data: [this.maxValue],
