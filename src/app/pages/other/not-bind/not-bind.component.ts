642fcdbc42abcfdc59ce45a51b7a33ddee97ea42
git <842101237@qq.com>
2018-08-13 14:29:31 +0800

    08-13

---
+++ src/app/pages/other/not-bind/not-bind.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { AppService } from '../../../app.service';
+@Component({
+    selector: 'app-not-bind',
+    templateUrl: './not-bind.component.html',
+    styleUrls: ['./not-bind.component.css']
+})
+export class NotBindComponent implements OnInit {
+    public language;
+    constructor(private service: AppService, ) { }
+
+    ngOnInit() {
+        this.language = localStorage.getItem("language");
+        if (this.service.accessControl(new window_obj(), "notBind")) return;
+    }
+
+}
